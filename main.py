4e814db9951f80782ef6f6d00bed3608d10fe2d1
mouseorcleg <torindubozit@gmail.com>
2022-11-30 19:07:37 +0000

    first test passed!

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
+class Truck:
 
+    def __init__(self, model):
+        self.model = model
--- test_main.py
+++ test_main.py
@@ -1,1 +1,5 @@
+from main import Truck
 
+def test_create_a_new_truck():
+    truck1 = Truck("Small")
+    assert truck1.model == "Small"
