fa1a532299218a174d041bd67554cb5d04d8d9a9
Clarice R <57894922+ClariceR@users.noreply.github.com>
2022-11-30 19:35:07 +0000

    Update test_main.py
    testing interface

--- test_main.py
+++ test_main.py
@@ -1,5 +1,13 @@
-from main import Truck
+from main import Truck, Ship
 
 def test_create_a_new_truck():
     truck1 = Truck("Small")
     assert truck1.model == "Small"
+
+def test_create_a_new_ship():
+    ship1 = Ship("Big")
+    assert ship1.model == "Big"
+
+def test_can_truck_deliver():
+    truck1 = Truck("Medium")
+    assert truck1.deliver()
